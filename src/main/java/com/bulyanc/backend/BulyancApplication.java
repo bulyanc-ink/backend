799aa94195a42f12df022af340baa4af71e64d67
yukong <kyb062666@gmail.com>
2024-10-03 18:14:16 +0900

    [chore] 기본 스프링 프로젝트 셋팅

---
+++ src/main/java/com/bulyanc/backend/BulyancApplication.java
@@ -0,0 +1,13 @@
+package com.bulyanc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulyancApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulyancApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bulyanc/backend/BulyancApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulyanc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulyancApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
